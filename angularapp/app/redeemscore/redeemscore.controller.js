0bf736f808001efff568abdd9526a8b7282a0da4
tekdungtralala <tekdung554@gmail.com>
2015-09-18 21:09:04 +0700

    Improve Page Daftar Redeem Pemain

--- angularapp/app/redeemscore/redeemscore.controller.js
+++ angularapp/app/redeemscore/redeemscore.controller.js
@@ -5,20 +5,28 @@
 		.module('app.redeemscore')
 		.controller('RedeemScore', RedeemScore);
 
-	function RedeemScore($filter, dataservice) {
+	function RedeemScore($filter, dataservice, $scope) {
 		var allrow = [];
 		var vm = this;
 		vm.tab = 'group-by-user'; // group-by-user/all-row
 		vm.tab = 'all-row';
 		vm.users = [];
-		vm.showAllrow = [];
+		vm.allRowInTable = [];
+		vm.rowInPage = [];
 		vm.allRowFilter = {};
 		vm.totalSortOpt = true;
+
+		vm.pageOpt = {};
+		vm.pageOpt.currentPage = 1;
+		vm.pageOpt.maxSize = 5;
+		vm.pageOpt.itemPerPage = 15;
 
 		vm.changeTab = changeTab;
 		vm.filterByItemName = filterByItemName;
 		vm.filterByName = filterByName;
 		vm.toggleTotalSort = toggleTotalSort;
+		vm.changePage = changePage;
+		vm.resetFilter = resetFilter;
 
 		activate();
 		function activate() {
@@ -26,22 +34,60 @@
 				.then(afterRedeemScore);
 		}
 
+		function resetFilter() {
+			vm.allRowFilter.name = '';
+			vm.allRowFilter.itemName = '';
+			filterByName();
+		}
+
+		function changePage() {
+			updateRowInPage();
+		}
+
+		function updatePagOpt() {
+			vm.pageOpt.totalItems = vm.allRowInTable.length;
+			vm.pageOpt.currentPage = 1;
+			updateRowInPage();
+		}
+
+		function updateRowInPage() {
+			var itemPerPage = vm.pageOpt.itemPerPage;
+			var totalItems = vm.allRowInTable.length;
+			var start = (vm.pageOpt.currentPage * itemPerPage) - itemPerPage;
+			var end = (vm.pageOpt.currentPage * itemPerPage);
+			end = totalItems < end ? totalItems : end;
+
+			vm.rowInPage = _.slice(vm.allRowInTable, start, end);
+		}
+
 		function toggleTotalSort() {
 			vm.totalSortOpt = !vm.totalSortOpt;
+			var tmp = _.sortBy(vm.allRowInTable, function(d) {
+				return d.total * (vm.totalSortOpt ? 1 : -1);
+			});
+
+			vm.allRowInTable = tmp;
+			updateRowInPage();
 		}
 
 		function filterByName() {
+			vm.allRowFilter.itemName = '';
 			var name = vm.allRowFilter.name;
-			vm.showAllrow = _.filter(allrow, function(r) {
+			vm.allRowInTable = _.filter(allrow, function(r) {
 				return r.nameLower.indexOf(name) > -1;
 			});
+
+			updatePagOpt();
 		}
 
 		function filterByItemName() {
+			vm.allRowFilter.name = '';
 			var itemName = vm.allRowFilter.itemName;
-			vm.showAllrow = _.filter(allrow, function(r) {
+			vm.allRowInTable = _.filter(allrow, function(r) {
 				return r.itemNameLower.indexOf(itemName) > -1;
 			});
+
+			updatePagOpt();
 		}
 
 		function changeTab(tab) {
@@ -49,9 +95,19 @@
 		}
 
 		function afterRedeemScore(result) {
+			// var length = result.length;
+			// for(var j=0;j<9;j++)	{
+			// 	for(var i=0;i<length;i++) {
+			// 		var r = angular.copy(result[i]);
+			// 		r[0] = i+j;
+			// 		r[1] += ' - ' + i;
+			// 		result.push(r);
+			// 	}
+			// }
+
 			// init all row
 			allrow = [];
-			vm.showAllrow = [];
+			vm.allRowInTable = [];
 			_.each(result, function(r) {
 				var newData = {
 					id: r[0],
@@ -75,7 +131,8 @@
 				return true;
 			});
 
-			vm.showAllrow = angular.copy(allrow);
+			vm.allRowInTable = angular.copy(allrow);
+			updatePagOpt();
 
 			// init table group-by-user
 			vm.users = [];
